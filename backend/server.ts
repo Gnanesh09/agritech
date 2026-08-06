92c64525ca63fc367c092be2e6648c22af8f5b7e
Gnanesh G <gnanesh9206@gmail.com>
2026-08-06 12:27:07 +0530

    feat: set up Dockerfile and .dockerignore for backend environment

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,15 @@
 import app from "./src/app";
 import connectDB from "./src/db/db";
 
-connectDB();
-const PORT =
-  process.env.NODE_ENV === "production"
-    ? Number(process.env.PORT) || 3000
-    : 3050;
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const PORT = Number(process.env.PORT) || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on PORT: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to start server:", error);
+    process.exit(1);
   });
-}
