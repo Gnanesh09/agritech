65e8118ff6d5d1eddb9c296a506ee7deb0827556
Gnanesh G <gnanesh9206@gmail.com>
2026-08-08 18:50:34 +0530

    minor chnages

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -201,7 +201,7 @@ export default function AuthPage() {
   // ---------------------------------------------------------
 
   return (
-    <div className="min-h-screen bg-brand-light flex items-center justify-center px-4">
+    <div className="min-h-screen bg-white flex items-center justify-center px-4">
       <div className="w-full max-w-md">
         {/* ===================================================
             HEADER
--- web/next.config.ts
+++ web/next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+
+module.exports = {
+  allowedDevOrigins: ['10.192.25.210'],
+}
 const nextConfig: NextConfig = {
   
 };
